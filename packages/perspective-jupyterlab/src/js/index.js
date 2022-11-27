9db0518d76c86570e0ffcfd575ebf23b9dbb34ef
greeb <gr44bli@gmail.com>
2022-11-27 20:32:28 +0100

    renames

--- packages/perspective-jupyterlab/src/js/index.js
+++ packages/perspective-jupyterlab/src/js/index.js
@@ -15,9 +15,9 @@ export * from "./widget";
 
 /* css */
 import "../less/index.less";
-import "@finos/perspective-viewer-datagrid";
-import "@finos/perspective-viewer-d3fc";
-import "@finos/perspective-viewer-openlayers/dist/umd/perspective-viewer-openlayers.js";
+import "@shortex/perspective-viewer-datagrid";
+import "@shortex/perspective-viewer-d3fc";
+import "@shortex/perspective-viewer-openlayers/dist/umd/perspective-viewer-openlayers.js";
 import {perspectiveRenderers} from "./renderer";
 import {PerspectiveJupyterPlugin} from "./plugin";
 
